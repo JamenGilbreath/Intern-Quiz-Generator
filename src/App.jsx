01ec23a4902a9f48f77ec64f1d82594618215765
Soraya <60835721+sorayaboza@users.noreply.github.com>
2023-10-04 16:24:33 -0700

    added css and items for header, but currently non-functional. waiting to see if i can use react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./pages/Header.jsx"
+import React from 'react';
 
 function App() {
-  return <div>App</div>
+  return (
+    <div class="background">
+      <Header />
+      <div>App</div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Header.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+export default function Header() {
+    return (
+        <nav className="nav-bar">
+            <h1>INTERN QUIZ GENERATOR</h1>
+            <h2 id="nav-item">QUIZ</h2>
+            <h2 id="nav-item">SCORE</h2>
+        </nav>
+    )
+}
