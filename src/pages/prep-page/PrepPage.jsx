4996b83fb5f12945724c25947c4765f3f31cc060
Gome510 <bluehawkfire55@gmail.com>
2023-10-05 14:24:29 -0700

    Added Link and quizName properties

--- src/pages/prep-page/PrepPage.jsx
+++ src/pages/prep-page/PrepPage.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./PrepPage.css";
 
-function PrepPage() {
+function PrepPage({ quizName, link }) {
   const background = {
-    /* background:
-      "linear-gradient(322deg, rgba(6,0,113,1) 0%, rgba(4,21,162,1) 30%, rgba(123,232,254,1) 100%)",*/
-
     backgroundImage: 'url("/src/assets/images/cool-background.png")',
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
   };
+  const quiz = quizName ? quizName : "this Quiz";
 
   return (
     <div
@@ -21,10 +20,12 @@ function PrepPage() {
         className="bg-light rounded h-25 p-4 d-flex flex-column align-items-center"
         style={{ width: "50%", minWidth: "20rem" }}
       >
-        <h1 className="mb-4">Ready for quizName?</h1>
-        <Button size="lg" className="w-50 h-50">
-          Start Quiz
-        </Button>
+        <h1 className="mb-4">Ready for {quiz}?</h1>
+        <Link className="w-50 h-50" to={quiz}>
+          <Button size="lg" className="w-100 h-100">
+            Start Quiz
+          </Button>
+        </Link>
       </div>
     </div>
   );
